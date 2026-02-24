d1989379c9baa2461a52e4f978bd95080380d964
fayas17 <fayas1771@gmail.com>
2026-02-24 11:05:51 +0530

    add config.py for application settings management using Pydantic

---
+++ app/core/config.py
@@ -0,0 +1,46 @@
+from pydantic_settings import BaseSettings
+from functools import lru_cache
+
+
+class Settings(BaseSettings):
+    # App
+    APP_NAME: str
+    DEBUG: bool
+
+    # Database
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
+    DB_SCHEMA: str
+
+    # Redis
+    REDIS_HOST: str
+    REDIS_PORT: int
+
+    # Celery
+    CELERY_BROKER_URL: str
+    CELERY_RESULT_BACKEND: str
+
+    # JWT
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    @property
+    def DATABASE_URL(self):
+        return (
+            f"postgresql+psycopg2://{self.POSTGRES_USER}:"
+            f"{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:"
+            f"{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        )
+
+    class Config:
+        env_file = ".env"
+        case_sensitive = True
+
+
+@lru_cache()
+def get_settings():
+    return Settings()
