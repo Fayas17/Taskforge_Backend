9d0933654097b31dcd15b3bfd974412ff9a5c7d4
fayas17 <fayas1771@gmail.com>
2026-02-25 16:41:05 +0530

    add startup event to create schemas and initialize database

--- app/auth_service/main.py
+++ app/auth_service/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from app.core.database import engine, Base, create_schemas
+
+import app.auth_service.models
 
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    create_schemas()
+    Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def health():
