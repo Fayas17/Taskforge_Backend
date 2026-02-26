2aec7fe02c498c6c308551b7bdc32850c6ab81bb
fayas17 <fayas1771@gmail.com>
2026-02-26 12:21:34 +0530

    add router inclusion to main application setup

--- app/auth_service/main.py
+++ app/auth_service/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
 from app.core.database import engine, Base, create_schemas
 
+from app.auth_service import router
 import app.auth_service.models
 
 app = FastAPI()
+
+
+
+app.include_router(router.router)
+
 
 @app.on_event("startup")
 def on_startup():
