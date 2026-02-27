c2f9dc66eaff9c24b8a51d2f291be8322a5be440
fayas17 <fayas1771@gmail.com>
2026-02-27 17:49:55 +0530

    add RefreshToken model methods for token management

--- app/auth_service/repository.py
+++ app/auth_service/repository.py
@@ -1,5 +1,7 @@
+from datetime import datetime
+
 from sqlalchemy.orm import Session
-from .models import User
+from .models import User, RefreshToken
 from uuid import UUID
 
 
@@ -18,3 +20,27 @@ def create_user(db: Session, user_data: dict):
     db.commit()
     db.refresh(user)
     return user
+
+def create_refresh_token(db: Session, user_id: UUID, jti: str, expires_at):
+    refresh_token = RefreshToken(
+        user_id=user_id,
+        jti=jti,
+        expires_at=expires_at
+    )
+    db.add(refresh_token)
+    db.commit()
+    db.refresh(refresh_token)
+    return refresh_token
+
+def get_refresh_token(db: Session, jti: str):
+    return db.query(RefreshToken).filter(
+        RefreshToken.jti == jti,
+        RefreshToken.is_revoked == False,
+        RefreshToken.expires_at > datetime.utcnow()
+        ).first()
+
+def revoke_refresh_token(db: Session, jti: str):    
+    token = db.query(RefreshToken).filter(RefreshToken.jti == jti).first()
+    if token:
+        token.is_revoked = True
+        db.commit()
