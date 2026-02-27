62373ea09a9c88df3b533830ba4a93a22cd0b2a1
fayas17 <fayas1771@gmail.com>
2026-02-27 16:27:48 +0530

    add RefreshToken model and establish relationship with User model

--- app/auth_service/models.py
+++ app/auth_service/models.py
@@ -1,7 +1,10 @@
+import datetime
 import uuid
-from sqlalchemy import Column, String, Boolean, DateTime
+from sqlalchemy import Column, String, Boolean, DateTime, Integer, ForeignKey 
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
+
 from app.core.database import Base
 
 
@@ -16,3 +19,15 @@ class User(Base):
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+    refresh_token = relationship("RefreshToken", back_populates="users")
+
+class RefreshToken(Base):
+    __tablename__ = "refresh_tokens"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("auth.users.id"))
+    jti = Column(String, unique=True, nullable=False)
+    expires_at = Column(DateTime(timezone=True), nullable=False)
+    is_revoked = Column(Boolean, default=False)
+    created_at = Column(DateTime(timezone=True), default=datetime.utcnow)
+    user = relationship("User", back_populates="refresh_tokens")
