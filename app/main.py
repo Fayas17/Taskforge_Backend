19f7665a8faed6de35a41370cdecdb4a51a198b8
fayas17 <fayas1771@gmail.com>
2026-03-02 12:34:20 +0530

    updated the according to the warning while testing

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from app.modules.auth.router import router as auth_router
@@ -11,10 +12,12 @@ app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 # app.include_router(jobs_router, prefix="/jobs", tags=["Jobs"])
 
 
-@app.on_event("startup")
-def on_startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     create_schemas()
     Base.metadata.create_all(bind=engine)
+    yield
+    Base.metadata.drop_all(bind=engine)
 
 
 @app.get("/")
--- app/modules/auth/schemas.py
+++ app/modules/auth/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, ConfigDict
 from uuid import UUID
 from datetime import datetime
 
@@ -19,5 +19,4 @@ class UserResponse(BaseModel):
     is_active: bool
     created_at: datetime
 
-    class Config:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
