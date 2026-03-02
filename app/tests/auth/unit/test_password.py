b6f1436c1b58212d927decf1fd8f80b166bd2946
fayas17 <fayas1771@gmail.com>
2026-03-02 16:08:40 +0530

    password hasing, verifying and wrong password testing implemented

---
+++ app/tests/auth/unit/test_password.py
@@ -0,0 +1,24 @@
+from app.modules.auth.service import hash_password, verify_password
+
+#password hashing tests
+def test_hash_password():
+
+    password = "Strong123!"
+    hashed = hash_password(password)
+
+    assert hashed != password
+
+#hashed pasword verify test
+def test_verify_password():
+    password = "Strong123!"
+    hashed = hash_password(password)
+
+    assert verify_password(password, hashed) == True
+
+#Wrong password verify test
+def test_verify_wrong_password():
+    password = "Strong123!"
+    wrong_password = "WrongPass123!"
+    hashed = hash_password(password)
+
+    assert verify_password(wrong_password, hashed) == False
